9c3f95059c9b5846d4fd558f44f310aaeefac60d
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-13 13:40:06 +0530

    added exception handling with global excpetions

---
+++ src/main/java/com/pramdvaddiraju/notifyflow_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.pramdvaddiraju.notifyflow_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    ResponseEntity<Map<String,Object>> handleResourceNotFoundException(ResourceNotFoundException ex){
+        Map<String,Object> response = new HashMap<>();
+
+        response.put("Time Stamp: ", LocalDateTime.now());
+        response.put("Message: ", ex.getMessage());
+        response.put("Status: ", HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<Map<String,Object>> handleValidationError(MethodArgumentNotValidException ex){
+        Map<String,Object> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+}
---
+++ src/main/java/com/pramdvaddiraju/notifyflow_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.pramdvaddiraju.notifyflow_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+
+
+}
