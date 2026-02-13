55caec5bab12d5c9273e0f2b94fd1ba1ab104ebf
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-13 12:56:30 +0530

    initial commit

---
+++ src/main/java/com/pramdvaddiraju/notifyflow_backend/NotifyflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pramdvaddiraju.notifyflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramdvaddiraju/notifyflow_backend/NotifyflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramdvaddiraju.notifyflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
